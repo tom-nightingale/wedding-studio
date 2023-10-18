6a5010301771c2177c276f593cf6f8367a32b7d4
Tom Nightingale <tom@weareleague.co.uk>
2023-10-18 19:16:47 +0100

    feat(Studio): Refactor studio

--- schemas/singletonHome.ts
+++ schemas/singletonHome.ts
@@ -17,6 +17,16 @@ export default defineType({
   ],
   fields: [
     defineField({
+      name: 'heroImage',
+      title: 'Hero Image',
+      description: 'Hero image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      group: 'content',
+    }),
+    defineField({
       name: 'sections',
       title: 'Sections',
       type: 'array',
